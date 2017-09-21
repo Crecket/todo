3edd8684be126568dc6e417efd7bc6f110a4b085
Gregory Goijaerts <crecketgaming@gmail.com>
2017-09-21 12:03:46 +0200

    Fixed outdated error controller not extending parent controller class

--- src/Controllers/ErrorController.php
+++ src/Controllers/ErrorController.php
@@ -4,27 +4,27 @@ namespace Greg\ToDo\Controllers;
 
 use Greg\ToDo\Http\Response;
 
-class ErrorController
+class ErrorController extends Controller
 {
 
-    public static function error500(\Twig_Environment $twig, \Exception $exception)
+    public function error500(\Twig_Environment $twig, \Exception $exception)
     {
         return new Response($twig->render("errors/error500.twig", array(
             "exception" => $exception->getMessage()
         )), 500);
     }
 
-    public static function error404(\Twig_Environment $twig)
+    public function error404(\Twig_Environment $twig)
     {
         return new Response($twig->render("errors/error404.twig"), 404);
     }
 
-    public static function error403(\Twig_Environment $twig)
+    public function error403(\Twig_Environment $twig)
     {
         return new Response($twig->render("errors/error403.twig"), 403);
     }
 
-    public static function error400(\Twig_Environment $twig)
+    public function error400(\Twig_Environment $twig)
     {
         return new Response($twig->render("errors/error400.twig"), 400);
     }
