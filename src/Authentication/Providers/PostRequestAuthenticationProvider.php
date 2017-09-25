05ad98b58559f50ef6781345f50988a0089ec786
Gregory Goijaerts <crecketgaming@gmail.com>
2017-09-25 18:45:07 +0200

    Modified post request provider to use hashing

--- src/Authentication/Providers/PostRequestAuthenticationProvider.php
+++ src/Authentication/Providers/PostRequestAuthenticationProvider.php
@@ -25,7 +25,7 @@ class PostRequestAuthenticationProvider extends Provider
             return false;
         }
 
-        if ($user->password !== $_POST['password']) {
+        if (!password_verify($_POST['password'], $user->password)) {
             return false;
         }
 
